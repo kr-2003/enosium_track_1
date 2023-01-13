37340edea22094b8a12c952424df06a414b6aa24
kr-2003 <kumar.kr.abhinav@gmail.com>
2023-01-13 11:10:49 +0530

    Frontend:LoginRegisterAdded

---
+++ app/pages/login.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Login from '../components/Login'
+import Header from '../components/Header'
+
+function register() {
+  return (
+    <div>
+        <Header></Header>
+        <Login></Login>
+    </div>
+  )
+}
+
+export default register
---
+++ app/pages/register.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Register from '../components/Register'
+import Header from '../components/Header'
+
+function register() {
+  return (
+    <div>
+        <Header></Header>
+        <Register></Register>
+    </div>
+  )
+}
+
+export default register
